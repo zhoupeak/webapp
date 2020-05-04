3a5d07ce7917d000dd1a2480b0960c8589c291f5
zhoupeak <123zhou456yao>
2020-05-04 20:05:13 +0800

    test 更新2

--- my_app_test/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyAppTestConfig(AppConfig):
-    name = 'my_app_test'
--- webapp/settings.py
+++ webapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booktest', #注册应用
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,20 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'visualization',
+        'USER': 'root',
+        'PASSWORD': 'uCMZiyD/uCKWCv7a',
+        'HOST': 'offline-tech.ikunchi.com',
+        'PORT': '40750'
+        # 'NAME': 'visualization',
+        # 'USER': 'root',
+        # 'PASSWORD': '123456',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
+
     }
 }
 
